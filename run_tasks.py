299f32aa0f18a863b3974227046c9d29cc23644d
WuonParticle <WuonParticle@gmail.com>
2025-04-07 17:32:00 -0600

    manually set log configuration to have date as logging_format.yml not working

--- run_tasks.py
+++ run_tasks.py
@@ -1,6 +1,11 @@
 import asyncio
 import argparse
-from core.task_runner import TaskRunner
+import logging.config
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Run tasks from configuration')
@@ -10,6 +15,7 @@ def parse_args():
     return parser.parse_args()
 
 async def main():
+    from core.task_runner import TaskRunner
     args = parse_args()
     runner = TaskRunner(config_path=args.config)
     await runner.run()
